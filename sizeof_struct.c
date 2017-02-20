ddcef97764efb09001678d5c1aa6a16ad1b755b0
farwish <farwish@foxmail.com>
2017-02-21 00:51:35 +0800

    sizeof struct

--- sizeof_struct.c
+++ sizeof_struct.c
@@ -24,18 +24,18 @@ int main(void)
     printf("sizeof(today)   = %ld\n", sizeof(today));
     printf("sizeof(tomorrow)= %ld\n", sizeof(tomorrow));
 
-    printf("*(today.month)  = %p\n", &today.month);
-    printf("*(today.day)    = %p\n", &(today.day));
-    printf("*(today.year)   = %p\n", &(today.year));
+    printf("&(today.month)  = %p\n", &today.month);
+    printf("&(today.day)    = %p\n", &(today.day));
+    printf("&(today.year)   = %p\n", &(today.year));
 
     printf("sizeof(p)       = %ld\n", sizeof(p));
     printf("sizeof(int)     = %ld\n", sizeof(int));
     printf("sizeof(double)  = %ld\n", sizeof(double));
     printf("sizeof(char)    = %ld\n", sizeof(char));
 
-    printf("*(p.x)          = %p\n", &(p.x));
-    printf("*(p.y)          = %p\n", &(p.y));
-    printf("*(p.z)          = %p\n", &(p.z));
+    printf("&(p.x)          = %p\n", &(p.x));
+    printf("&(p.y)          = %p\n", &(p.y));
+    printf("&(p.z)          = %p\n", &(p.z));
 
     return 0;
 }
