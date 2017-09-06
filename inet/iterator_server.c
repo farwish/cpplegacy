741770860c2a0325f722d3e4b56ece1b6ebc2e6b
farwish <farwish@foxmail.com>
2017-09-06 21:21:41 +0800

    ..

---
+++ inet/iterator_server.c
@@ -0,0 +1,16 @@
+/**
+ * 迭代服务器通常指单进程阻塞服务器。
+ *
+ * INADDR_ANY == 0.0.0.0 表示所有地址
+ * 如果多个网卡，服务器可以在任一网络接口上接受请求，只区分端口号
+ *
+ * PF_UNIX, PF_INET
+ * "The protocol family generally is the same as the address family"
+ * AF_UNIX, AF_INET
+ *
+ * @farwish
+ */
+
+
+
+
