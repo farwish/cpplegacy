9f9cd27bc52b4f014a57daff291fe6d7f8e54888
farwish <farwish@foxmail.com>
2017-12-27 00:19:56 +0800

    Const member function, const object

---
+++ cpp/Const_class.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @license Apache-2.0
+ * @author farwish <farwish@foxmail.com>
+ */
+class A {
+
+private:
+    const int i;
+
+public:
+
+    A() : i(0) {
+    }
+
+    void f() {
+        cout << "A::f()" << endl;
+    }
+
+    void f() const {
+        cout << "A::f() const" << endl;
+    }
+};
+
+int main()
+{
+    A a;
+    a.f(); // A::f()
+
+    const A a2;
+    a2.f(); // A::f() const
+
+    return 0;
+}
